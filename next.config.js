ab7b69adc13abcad08aebb9915dfbc2992cb790f
gelarahadian <103013198+gelarahadian@users.noreply.github.com>
2023-02-10 08:11:44 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
   images: {
     domains: ['links.papareact.com'],
   },
-  env: {
-    MAPBOX_ACCESS_TOKEN:
-      'pk.eyJ1IjoiZ2VsYXJhaGFkaWFuIiwiYSI6ImNsZHdxZ3Y4ZzBhNzQzcXBrNGN5aWZ1MXgifQ.n9HKXanmhId7aTz2E6_c7Q',
-  },
 };
 
 module.exports = nextConfig;
